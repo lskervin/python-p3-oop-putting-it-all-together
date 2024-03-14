322eda95df2a98cf05cac1be81c457cd7222deed
LeightonSkervin <lskervin23@gmail.com>
2024-03-14 12:46:49 -0400

    lab complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
-
+# the title and page_count
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
     
-        
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, new_title):
+        if isinstance(new_title, str):
+            self._title = new_title    
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if isinstance(new_page_count, int):
+            self._page_count = new_page_count
+            return self._page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if isinstance(new_size, int):
+            self._size = new_size
+            return self._size
+        else:
+            print("size must be an integer")
+       
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+        # return self._condition
+        # if cobble.has_been_called:
+    #         
+# my_shoe = Shoe(brand="Nike", size=9, condition="New")
+        
+
+        
